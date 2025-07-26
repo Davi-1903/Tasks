f78a7a7fb8dd6e3c071af466328010f5172a6194
Davi <franciscodavi327@gmail.com>
2025-07-26 12:56:06 -0300

    Adicionar templates

--- src/app.py
+++ src/app.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
 
+# ================================================ ROTAS ================================================
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
