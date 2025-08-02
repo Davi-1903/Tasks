e2174198ce5bb3a5cac6d05cff5b5f1c8eef340c
Davi <franciscodavi327@gmail.com>
2025-08-02 09:43:01 -0300

    Adicionar página para o erro 500

--- src/config.py
+++ src/config.py
@@ -23,3 +23,8 @@ def config_app(app):
     @app.errorhandler(404)
     def page_not_found(error):
         return render_template('errors/404.html'), 404
+    
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('errors/500.html'), 500
